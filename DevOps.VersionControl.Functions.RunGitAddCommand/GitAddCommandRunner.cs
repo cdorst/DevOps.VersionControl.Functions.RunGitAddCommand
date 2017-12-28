7a4dc977e69c19ffefc207ababac1e34129b1dbb
Christopher Dorst <chris@dorst.email>
2017-12-27 20:18:17 -0600

    Adding GitAddCommandRunner

---
+++ DevOps.VersionControl.Functions.RunGitAddCommand/GitAddCommandRunner.cs
@@ -0,0 +1,15 @@
+﻿using Common.EnumStringValues;
+using static DevOps.VersionControl.Functions.RunGitCommand.GitCommandRunner;
+
+namespace DevOps.VersionControl.Functions.RunGitAddCommand
+{
+    public static class GitAddCommandRunner
+    {
+        private const string add = nameof(add);
+
+        public static void Add(string directory, StageType stageType = StageType.AllChanges)
+            => Git(directory,
+                command: add,
+                stageType.GetStringValue());
+    }
+}
