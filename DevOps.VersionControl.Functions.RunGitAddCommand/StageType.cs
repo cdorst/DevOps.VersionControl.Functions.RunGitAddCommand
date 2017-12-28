2c5b629e282e7b44bb26783efd9f0825e0babdd9
Christopher Dorst <chris@dorst.email>
2017-12-27 20:18:12 -0600

    Adding staging type constants and enum types

---
+++ DevOps.VersionControl.Functions.RunGitAddCommand/StageType.cs
@@ -0,0 +1,14 @@
+﻿using Common.EnumStringValues;
+
+namespace DevOps.VersionControl.Functions.RunGitAddCommand
+{
+    public enum StageType : byte
+    {
+        [EnumStringValue(StagingConstants.AllChanges)]
+        AllChanges,
+        [EnumStringValue(StagingConstants.OnlyNewAndModified)]
+        OnlyNewAndModified,
+        [EnumStringValue(StagingConstants.OnlyModifiedAndDeleted)]
+        OnlyModifiedAndDeleted
+    }
+}
---
+++ DevOps.VersionControl.Functions.RunGitAddCommand/StagingConstants.cs
@@ -0,0 +1,9 @@
+﻿namespace DevOps.VersionControl.Functions.RunGitAddCommand
+{
+    internal class StagingConstants
+    {
+        public const string AllChanges = "-A";
+        public const string OnlyNewAndModified = ".";
+        public const string OnlyModifiedAndDeleted = "-u";
+    }
+}
